90ede0b83b11ba2612402c85315a230800de1c12
hilotter <hilotter3@gmail.com>
2020-02-23 12:54:49 +0900

    remove mongo

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-var express = require('express'),
-    bonnou = require('./routes/bonnou');
+var express = require("express"),
+  bonnou = require("./routes/bonnou");
 
 var app = express();
 
-app.configure(function () {
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.static(__dirname + '/public'));
-});
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-app.get('/favicon.ico', function(req, res){
-    res.end();
-    return;
+app.get("/favicon.ico", function(req, res) {
+  res.end();
+  return;
 });
-app.get('/api', bonnou.findAll);
-app.get('/api/:id', bonnou.findById);
-app.get('*', function(req, res){
-    res.send({error: "bonnou not found"}, 404);
+app.get("/api", bonnou.findAll);
+app.get("/api/:id", bonnou.findById);
+app.get("*", function(req, res) {
+  res.status(404).send({ error: "bonnou not found" });
 });
 
 app.listen(process.env.PORT || 3000);
-console.log('Listening on port #{process.env.PORT}...');
+console.log(`Listening on port ${process.env.PORT}...`);
